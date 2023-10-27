e26cef87a5c1dda50f6c0a13d6aaea31c943eb6f
Bhaskar <bhaskaragodasi@gmail.com>
2023-10-27 12:43:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const sam = 5;
+
+const arr = [1,2, 3,4,5,6];
+
+arr.push(1)
+
+// sam or arr
+
+const arr1=arr.slice(1, 4)
+console.log(arr1)
+
+// splice
+
+arr.splice(2,1,10,11)
+console.log(arr)
+
+
+// map , filter,
+
+
